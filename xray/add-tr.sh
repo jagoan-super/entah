aaf7c347aafda92c44f31726110c091c41f58154
jagoan-super <127441477+jagoan-super@users.noreply.github.com>
2023-06-14 22:34:44 +0700

    Update add-tr.sh

--- xray/add-tr.sh
+++ xray/add-tr.sh
@@ -113,7 +113,6 @@ echo -e "======-XRAYS/TROJAN-======" | tee -a /etc/log-create-user.log
 echo -e "==========================" | tee -a /etc/log-create-user.log
 echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
 echo -e "Host/IP : ${domain}" | tee -a /etc/log-create-user.log
-echo -e "Wildcard : (bug.com).${domain}" | tee -a /etc/log-create-user.log
 echo -e "Port TLS : ${tls}" | tee -a /etc/log-create-user.log
 echo -e "Port none TLS : ${ntls}" | tee -a /etc/log-create-user.log
 echo -e "Port gRPC : ${tls}" | tee -a /etc/log-create-user.log
